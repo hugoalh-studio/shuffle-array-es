1762e85aec195d4d4109f4afd03e2fae35c7444f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-06-16 16:01:09 +0800

    Initial

---
+++ mod.ts
@@ -0,0 +1,23 @@
+import { randomInt } from "node:crypto";
+/**
+ * @function shuffleArray
+ * @description Shuffle the array's indexes.
+ * @template {unknown} T
+ * @param {T[]} item Array that need to shuffle indexes.
+ * @returns {T[]} An indexes shuffled array.
+ */
+function shuffleArray<T>(item: T[]): T[] {
+	if (!Array.isArray(item)) {
+		throw new TypeError(`Argument \`item\` must be type of array!`);
+	}
+	let itemClone: T[] = [...item];
+	let result: T[] = [];
+	while (itemClone.length > 1) {
+		let index: number = randomInt(0, itemClone.length);
+		result.push(itemClone[index]);
+		itemClone.splice(index, 1);
+	}
+	result.push(itemClone[0]);
+	return result;
+}
+export default shuffleArray;
