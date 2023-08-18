fb39fd71eafc10b688ff50dc0e8a2f706d6497ec
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-18 17:01:12 +0800

    Update resources

---
+++ mod.bench.ts
@@ -0,0 +1,7 @@
+import shuffleArray from "./mod.ts";
+Deno.bench({
+	name: "0",
+	fn: () => {
+		shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93])
+	}
+});
--- mod.test.ts
+++ mod.test.ts
@@ -1,41 +1,68 @@
 import { assertThrows } from "https://deno.land/std@0.198.0/assert/assert_throws.ts";
 import shuffleArray from "./mod.ts";
-Deno.test("0", () => {
-	shuffleArray([]);
+Deno.test({
+	name: "P-0",
+	fn: () => {
+		shuffleArray([]);
+	}
 });
-Deno.test("1", () => {
-	shuffleArray([1n, 2n, 3n]);
+Deno.test({
+	name: "P-1",
+	fn: () => {
+		shuffleArray([1n, 2n, 3n]);
+	}
 });
-Deno.test("2", () => {
-	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
+Deno.test({
+	name: "P-2",
+	fn: () => {
+		shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
+	}
 });
-Deno.test("3", () => {
-	assertThrows(() => {
-		shuffleArray(64);
-	});
+Deno.test({
+	name: "F-1",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray(64);
+		});
+	}
 });
-Deno.test("4", () => {
-	assertThrows(() => {
-		shuffleArray(64n);
-	});
+Deno.test({
+	name: "F-2",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray(64n);
+		});
+	}
 });
-Deno.test("5", () => {
-	assertThrows(() => {
-		shuffleArray(false);
-	});
+Deno.test({
+	name: "F-3",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray(false);
+		});
+	}
 });
-Deno.test("6", () => {
-	assertThrows(() => {
-		shuffleArray(undefined);
-	});
+Deno.test({
+	name: "F-4",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray(undefined);
+		});
+	}
 });
-Deno.test("7", () => {
-	assertThrows(() => {
-		shuffleArray(null);
-	});
+Deno.test({
+	name: "F-5",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray(null);
+		});
+	}
 });
-Deno.test("8", () => {
-	assertThrows(() => {
-		shuffleArray({ foo: "bar" });
-	});
+Deno.test({
+	name: "F-6",
+	fn: () => {
+		assertThrows(() => {
+			shuffleArray({ foo: "bar" });
+		});
+	}
 });
