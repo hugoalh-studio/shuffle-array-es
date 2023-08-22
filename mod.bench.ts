054476d1837f4d9284e92058d2946932f1b1c252
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-22 17:36:52 +0800

    Update resources

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,7 +1,4 @@
 import shuffleArray from "./mod.ts";
-Deno.bench({
-	name: "1",
-	fn: () => {
-		shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
-	}
+Deno.bench("Main", () => {
+	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
--- mod.test.ts
+++ mod.test.ts
@@ -1,68 +1,41 @@
 import { assertThrows } from "https://deno.land/std@0.199.0/assert/assert_throws.ts";
 import shuffleArray from "./mod.ts";
-Deno.test({
-	name: "P-1",
-	fn: () => {
-		shuffleArray([]);
-	}
+Deno.test("Success 1", () => {
+	shuffleArray([]);
 });
-Deno.test({
-	name: "P-2",
-	fn: () => {
-		shuffleArray([1n, 2n, 3n]);
-	}
+Deno.test("Success 2", () => {
+	shuffleArray([1n, 2n, 3n]);
 });
-Deno.test({
-	name: "P-3",
-	fn: () => {
-		shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
-	}
+Deno.test("Success 3", () => {
+	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
-Deno.test({
-	name: "F-1",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray(64);
-		});
-	}
+Deno.test("Throw 1", () => {
+	assertThrows(() => {
+		shuffleArray(64);
+	});
 });
-Deno.test({
-	name: "F-2",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray(64n);
-		});
-	}
+Deno.test("Throw 2", () => {
+	assertThrows(() => {
+		shuffleArray(64n);
+	});
 });
-Deno.test({
-	name: "F-3",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray(false);
-		});
-	}
+Deno.test("Throw 3", () => {
+	assertThrows(() => {
+		shuffleArray(false);
+	});
 });
-Deno.test({
-	name: "F-4",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray(undefined);
-		});
-	}
+Deno.test("Throw 4", () => {
+	assertThrows(() => {
+		shuffleArray(undefined);
+	});
 });
-Deno.test({
-	name: "F-5",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray(null);
-		});
-	}
+Deno.test("Throw 5", () => {
+	assertThrows(() => {
+		shuffleArray(null);
+	});
 });
-Deno.test({
-	name: "F-6",
-	fn: () => {
-		assertThrows(() => {
-			shuffleArray({ foo: "bar" });
-		});
-	}
+Deno.test("Throw 6", () => {
+	assertThrows(() => {
+		shuffleArray({ foo: "bar" });
+	});
 });
