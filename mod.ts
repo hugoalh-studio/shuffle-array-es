290f68c622cf7f9e27d7e2aec0d776912499408f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-08-27 17:34:13 +0800

    Revert "Improve error message"
    This reverts commit 71df8ae296ad57280badfee3fdee31ca456a7ecc.

--- mod.ts
+++ mod.ts
@@ -7,7 +7,7 @@ import { randomInt } from "node:crypto";
  */
 export default function shuffleArray<T>(item: T[]): T[] {
 	if (!Array.isArray(item)) {
-		throw new TypeError(`Argument \`item\` must be T[]!`);
+		throw new TypeError(`Argument \`item\` must be type of array!`);
 	}
 	const itemClone: T[] = [...item];
 	const result: T[] = [];
