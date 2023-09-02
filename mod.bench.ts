91fbfd4e5a1f208fe7b46807046ab5195ab3edf7
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-02 11:16:28 +0800

    Remove base type guard

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,4 +1,10 @@
 import shuffleArray from "./mod.ts";
+Deno.bench("Empty", () => {
+	shuffleArray([]);
+});
+Deno.bench("3 Elements", () => {
+	shuffleArray([1n, 2n, 3n]);
+});
 Deno.bench("Example", () => {
 	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
--- mod.test.ts
+++ mod.test.ts
@@ -1,41 +1,13 @@
-import { assertThrows } from "https://deno.land/std@0.201.0/assert/assert_throws.ts";
 import shuffleArray from "./mod.ts";
-Deno.test("Success 1", () => {
+Deno.test("Empty", () => {
 	shuffleArray([]);
 });
-Deno.test("Success 2", () => {
+Deno.test("3 Elements", () => {
 	shuffleArray([1n, 2n, 3n]);
 });
-Deno.test("Success 3", () => {
+Deno.test("Example", () => {
 	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
-Deno.test("Throw 1", () => {
-	assertThrows(() => {
-		shuffleArray(64);
-	});
-});
-Deno.test("Throw 2", () => {
-	assertThrows(() => {
-		shuffleArray(64n);
-	});
-});
-Deno.test("Throw 3", () => {
-	assertThrows(() => {
-		shuffleArray(false);
-	});
-});
-Deno.test("Throw 4", () => {
-	assertThrows(() => {
-		shuffleArray(undefined);
-	});
-});
-Deno.test("Throw 5", () => {
-	assertThrows(() => {
-		shuffleArray(null);
-	});
-});
-Deno.test("Throw 6", () => {
-	assertThrows(() => {
-		shuffleArray({ foo: "bar" });
-	});
+Deno.test("15 Elements", () => {
+	shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
 });
