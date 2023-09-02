eedaa3c900943d32deb340b167275d7cdc5e2b6f
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-02 10:59:14 +0800

    Revert "Update test"
    This reverts commit 2f5fefdf399abf84d94a49d31b32d892f0c53e43.

--- mod.test.ts
+++ mod.test.ts
@@ -1,3 +1,4 @@
+import { assertThrows } from "https://deno.land/std@0.200.0/assert/assert_throws.ts";
 import shuffleArray from "./mod.ts";
 Deno.test("Success 1", () => {
 	shuffleArray([]);
@@ -7,4 +8,34 @@ Deno.test("Success 2", () => {
 });
 Deno.test("Success 3", () => {
 	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
+});
+Deno.test("Throw 1", () => {
+	assertThrows(() => {
+		shuffleArray(64);
+	});
+});
+Deno.test("Throw 2", () => {
+	assertThrows(() => {
+		shuffleArray(64n);
+	});
+});
+Deno.test("Throw 3", () => {
+	assertThrows(() => {
+		shuffleArray(false);
+	});
+});
+Deno.test("Throw 4", () => {
+	assertThrows(() => {
+		shuffleArray(undefined);
+	});
+});
+Deno.test("Throw 5", () => {
+	assertThrows(() => {
+		shuffleArray(null);
+	});
+});
+Deno.test("Throw 6", () => {
+	assertThrows(() => {
+		shuffleArray({ foo: "bar" });
+	});
 });
