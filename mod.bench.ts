25e5a4975522be93b47fd44df675d6823fa6adba
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-05 11:47:31 +0800

    Update test

--- mod.bench.ts
+++ mod.bench.ts
@@ -1,13 +1,13 @@
 import shuffleArray from "./mod.ts";
-Deno.bench("Empty", () => {
+Deno.bench("Empty", { permissions: "none" }, () => {
 	shuffleArray([]);
 });
-Deno.bench("3 Elements", () => {
+Deno.bench("3 Elements", { permissions: "none" }, () => {
 	shuffleArray([1n, 2n, 3n]);
 });
-Deno.bench("10 Elements", () => {
+Deno.bench("10 Elements", { permissions: "none" }, () => {
 	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
-Deno.bench("15 Elements", () => {
+Deno.bench("15 Elements", { permissions: "none" }, () => {
 	shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
 });
--- mod.test.ts
+++ mod.test.ts
@@ -1,13 +1,13 @@
 import shuffleArray from "./mod.ts";
-Deno.test("Empty", () => {
+Deno.test("Empty", { permissions: "none" }, () => {
 	shuffleArray([]);
 });
-Deno.test("3 Elements", () => {
+Deno.test("3 Elements", { permissions: "none" }, () => {
 	shuffleArray([1n, 2n, 3n]);
 });
-Deno.test("10 Elements", () => {
+Deno.test("10 Elements", { permissions: "none" }, () => {
 	shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
 });
-Deno.test("15 Elements", () => {
+Deno.test("15 Elements", { permissions: "none" }, () => {
 	shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
 });
