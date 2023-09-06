8f9a2c343c8247cdf53c50bdb4fc84a400cbc4a3
hugoalh <32359235+hugoalh@users.noreply.github.com>
2023-09-06 16:52:55 +0800

    Add inline example

--- mod.ts
+++ mod.ts
@@ -4,6 +4,14 @@ import { randomInt } from "node:crypto";
  * @template {unknown} T
  * @param {T[]} item Array that need to shuffle indexes.
  * @returns {T[]} An indexes shuffled array.
+ * @example
+ * const chain = [3, 7, 25, 26, 42, 62, 71, 76, 92, 93];
+ * 
+ * shuffleArray(chain);
+ * //=> [62, 93, 26, 42, 25, 7, 76, 71, 3, 92]
+ * 
+ * shuffleArray(chain);
+ * //=> [42, 3, 26, 62, 93, 7, 76, 25, 92, 71]
  */
 export function shuffleArray<T>(item: T[]): T[] {
 	const itemClone: T[] = [...item];
