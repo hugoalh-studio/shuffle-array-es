ae34c6b03bfd2e70b5844ab1e48d9648c03a5d13
hugoalh <hugoalh@gmail.com>
2024-11-15 17:51:45 +0800

    Update test

--- mod.test.ts
+++ mod.test.ts
@@ -2,17 +2,21 @@ import { assertEquals } from "STD/assert/equals";
 import { shuffleArray } from "./mod.ts";
 Deno.test("0 Elements", { permissions: "none" }, () => {
 	const output = shuffleArray([]);
+	console.log(output);
 	assertEquals(output.length, 0);
 });
 Deno.test("3 Elements", { permissions: "none" }, () => {
 	const output = shuffleArray([1n, 2n, 3n]);
+	console.log(output);
 	assertEquals(output.length, 3);
 });
 Deno.test("10 Elements", { permissions: "none" }, () => {
 	const output = shuffleArray([3, 7, 25, 26, 42, 62, 71, 76, 92, 93]);
+	console.log(output);
 	assertEquals(output.length, 10);
 });
 Deno.test("15 Elements", { permissions: "none" }, () => {
 	const output = shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+	console.log(output);
 	assertEquals(output.length, 15);
 });
